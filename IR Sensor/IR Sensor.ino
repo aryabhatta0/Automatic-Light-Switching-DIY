d3134104a31307a82a41f3591111fa292dbe991c
aryabhatta0 <84025367+aryabhatta0@users.noreply.github.com>
2021-06-24 13:27:23 +0530

    Add some comments

--- IR Sensor/IR Sensor.ino
+++ IR Sensor/IR Sensor.ino
@@ -4,9 +4,9 @@ int IR = 7;
 int LED = 13;
 
 void setup() {
-  pinMode(IR, INPUT);
+  pinMode(IR, INPUT);       //IR: Input for LED
   pinMode(LED, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(9600);       //Start the serial monitor
 }
 
 void loop() {
