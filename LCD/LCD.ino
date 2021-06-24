96796dd4a58fd379b61a62419eef73f0ec7a4954
aryabhatta0 <84025367+aryabhatta0@users.noreply.github.com>
2021-06-24 13:04:03 +0530

    Add comments for explanation of code

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -3,14 +3,14 @@
 #include <LiquidCrystal.h>
 
 int i = 0;
-LiquidCrystal lcd(1, 2, 4, 5, 6, 7);  //connected pins
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7);    //declare & initialize LCD  *Pins are as in the model*
 
 void setup() {
-  lcd.begin(16, 2);  //(16,2) is the dimension of LCD.
+  lcd.begin(16, 2);   //(16,2) is the dimension of LCD.
 }
 
 void loop() {
-  lcd.setCursor(0,0);       
+  lcd.setCursor(0,0);     //Make cursor poin to from where you wanna print things.      
   lcd.print("Testing 1");
   delay(1000);
   
