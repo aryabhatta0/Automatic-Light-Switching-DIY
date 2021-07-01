a06d6939bf57b9aa0396ff2c21850ffc41078088
aryabhatta0 <84025367+aryabhatta0@users.noreply.github.com>
2021-07-01 11:21:03 +0530

    Final Code

--- project_code.ino
+++ project_code.ino
@@ -1,19 +1,22 @@
-//Things to add: Serial Monitor?
-
-#include <LiquidCrystal.h>   //library for LCD commands
+#include <LiquidCrystal.h>  //library for LCD commands
 
 #define IR_f 13     //front IR Sensor
 #define IR_b 12     //back IR Sensor
 #define LED 8
  
-int count=0;   
+int count=0;        //for calculating no. of people
+int light_state=0;  //0 means light off; 1 light on
+
+//variables for time calculation
+unsigned long ledON;
+unsigned long ledOFF;
+unsigned long timeTaken;
 
 LiquidCrystal lcd(1, 2, 4, 5, 6, 7);   //declare & initialize LCD
 
-void setup()
+void setup() 
 {
-  lcd.begin(16, 2);     //dimensions of LCD
- 
+  lcd.begin(16, 2);     //declaring dimensions
   pinMode(IR_f, INPUT);
   pinMode(IR_b, INPUT);
   pinMode(LED, OUTPUT);
@@ -21,37 +24,59 @@ void setup()
 
 void enter_room(){
   count++;
-  lcd.setCursor(0, 1);   //start of second line
-  lcd.print(String("People in room:")+String(count));
+  lcd.setCursor(0, 0);   //start of second line
+  lcd.print(String("People In: ")+String(count)+String("   "));
 }
 
-void exit_room(){
+void exit_room()
+{
   count--;
-  lcd.setCursor(0, 1);   //start of second line
-  lcd.print(String("People in room:")+String(count));
+  lcd.setCursor(0, 0);   //start of second line
+  lcd.print(String("People In: ")+String(count)+String("   "));
 }
 
 void control_the_led()
-{  
+{
   if(count>0){
-    digitalWrite(LED, HIGH);
-    lcd.setCursor(0, 0);
+    if(light_state==0){   //if light off
+      digitalWrite(LED, HIGH);
+      ledON = millis();    //record the starting time
+      light_state=1;
+    }
+    
+    lcd.setCursor(0, 1);
     lcd.print("Light: ON       ");    //extra space at last, to overwrite anything underneath
   }
   else{
-    digitalWrite(LED, LOW);
-    lcd.setCursor(0, 0);
-    lcd.print("Light: OFF      ");
+    if(light_state==1){
+      digitalWrite(LED, LOW);
+      ledOFF = millis();     //record the ending time
+      light_state=0;
+
+      lcd.setCursor(0, 1);
+      lcd.print("Turning bulb off..");
+
+      timeTaken = (ledOFF-ledON)/1000;  //in sec
+      lcd.setCursor(0, 1);
+      lcd.print(String("Time: ") + String(timeTaken) + String("s") +String("         "));  
+      delay(1000);    
+    }
+    
+    else{
+      lcd.setCursor(0, 1);
+      lcd.print("Light: OFF      ");
+    }
   }
 }
 
 void loop() 
-{                                    /*There must be some overlaping reason b/w the 2 sensors.*/
+{                                    /* There must be some overlaping reason b/w the 2 sensors. */
   if(digitalRead(IR_f)==LOW){
     while(1)
     {
       if(digitalRead(IR_b)==LOW){
         enter_room();
+        delay(1000);
         break;
       }
       else if(digitalRead(IR_f)==HIGH){
@@ -61,11 +86,12 @@ void loop()
     }
   }
 
-  else if(digitalRead(IR_b)==LOW){
+  if(digitalRead(IR_b)==LOW){
     while(1)
     {
       if(digitalRead(IR_f)==LOW){
         exit_room();
+        delay(1000);
         break;
       }
       else if(digitalRead(IR_b)==HIGH){
@@ -75,5 +101,5 @@ void loop()
     }
   }
 
-  control_the_led();   
+   control_the_led();   
 }
